5b19109715174c10b92bbc2e266089b259fa6f1c
android2600 <95949373+android2600@users.noreply.github.com>
2023-06-15 17:53:32 +0800

    .

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React, { useState, useMemo, useCallback} from 'react';
-import ForceGraph3D  from 'react-force-graph-3d';
-import data from "./data_source/output.json"
+import React, { useState, useMemo, useCallback } from 'react';
+import ForceGraph3D from 'react-force-graph-3d';
+import data from "./data_source/output.json";
 
 const App = () => {
   const [graphData, setGraphData] = useState(data);
   const [filteredData, setFilteredData] = useState(JSON.parse(JSON.stringify(graphData)));
   const [selectedId, setSelectedId] = useState('0');
-  const [temp,setTemp]=useState("");
+  const [temp, setTemp] = useState("");
   const [uploadedFile, setUploadedFile] = useState(null);
 
   const handleFileUpload = (event) => {
@@ -21,20 +21,21 @@ const App = () => {
         setFilteredData(JSON.parse(JSON.stringify(jsonData)));
       };
       reader.readAsText(file);
-    };
+    }
   };
 
-  const handleSearch = () => {
+  const handleSearch = async () => {
     setSelectedId(temp);
-    setFilteredData(JSON.parse(JSON.stringify(graphData)))
+    setFilteredData(JSON.parse(JSON.stringify(graphData)));
+    setPrunedTree(()=>getPrunedTree()); // Update prunedTree after filtering
   };
 
   const nodesById = useMemo(() => {
     const nodesById = {};
-    filteredData.nodes.forEach(node => {
+    filteredData.nodes.forEach((node) => {
       nodesById[node.id] = { ...node, collapsed: node.id !== selectedId, childLinks: [] };
     });
-    filteredData.links.forEach(link => {
+    filteredData.links.forEach((link) => {
       nodesById[link.source].childLinks.push(link);
     });
     return nodesById;
@@ -48,7 +49,7 @@ const App = () => {
       visibleNodes.push(node);
       if (node.collapsed) return;
       visibleLinks.push(...node.childLinks);
-      node.childLinks.forEach(link => {
+      node.childLinks.forEach((link) => {
         const childNode = typeof link.target === 'object' ? link.target : nodesById[link.target];
         traverseTree(childNode);
       });
@@ -58,39 +59,39 @@ const App = () => {
     // eslint-disable-next-line
   }, [nodesById]);
 
-  const [prunedTree, setPrunedTree] = useState(getPrunedTree());
+  const [prunedTree, setPrunedTree] = useState(()=>getPrunedTree());
 
-  const handleNodeClick = useCallback(node => {
+  const handleNodeClick = useCallback((node) => {
     node.collapsed = !node.collapsed;
     setPrunedTree(getPrunedTree());
+    console.log("Node Process")
   }, [getPrunedTree]);
-  
+
   return (
     <div>
       <div>
         <input type="file" accept=".json" onChange={handleFileUpload} />
         {uploadedFile && <span>File uploaded: {uploadedFile.name}</span>}
         <input
-        type="text"
-        value={temp}
-        onChange={(e) => setTemp(e.target.value)}
-        placeholder="Search.."
+          type="text"
+          value={temp}
+          onChange={(e) => setTemp(e.target.value)}
+          placeholder="Search.."
         />
-      <button onClick={handleSearch}>Search</button>
+        <button onClick={handleSearch}>Search</button>
       </div>
-        <ForceGraph3D
+      <ForceGraph3D
         graphData={prunedTree}
         onNodeClick={handleNodeClick}
-        nodeLabel={node=>node.id}
-        onNodeDragEnd={node => {
+        nodeLabel={(node) => node.id}
+        onNodeDragEnd={(node) => {
           node.fx = node.x;
           node.fy = node.y;
           node.fz = node.z;
         }}
-        />
+      />
     </div>
   );
 };
 
 export default App;
-
